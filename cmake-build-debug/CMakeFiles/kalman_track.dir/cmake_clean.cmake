65fbb9f36b1a991af01159bc30b8e4a0d16cd4a4
VincentZhao1992 <zwz_tjut@163.com>
2020-03-25 21:20:23 +0800

    创建工程

---
+++ cmake-build-debug/CMakeFiles/kalman_track.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kalman_track.dir/main.cpp.obj"
+  "kalman_track.exe"
+  "kalman_track.exe.manifest"
+  "kalman_track.lib"
+  "kalman_track.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kalman_track.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
